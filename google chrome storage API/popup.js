9c7c4a85f062e7d3a76fef5686c4379dc01427ab
eckysaroyd <enyato98@gmail.com>
2020-02-01 09:53:30 +0530

    begginning add a storage data

--- google chrome storage API/popup.js
+++ google chrome storage API/popup.js
@@ -2,4 +2,10 @@ $(function() {
   $("#alias").keyup(function() {
     $("#name").html($("#alias").val());
   });
+  $("submit").click(function() {
+    var username = $("#alias").val();
+
+    /**how to store data in chrome**/
+    chrome.storage.sync.set({ username: username }, function() {});
+  });
 });
