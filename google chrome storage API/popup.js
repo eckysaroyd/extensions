a9fa9b965b8b142d7eb4a946b55abdbb639be591
eckysaroyd <enyato98@gmail.com>
2020-02-01 10:14:55 +0530

    new change

--- google chrome storage API/popup.js
+++ google chrome storage API/popup.js
@@ -12,6 +12,6 @@ $(function() {
     var username = $("#alias").val();
 
     /**how to store data in chrome**/
-    chrome.storage.sync.set({ "username":username,}, function() {});
+    chrome.storage.sync.set({ username: username }, function() {});
   });
 });
